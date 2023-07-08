17538b5162e4189eaac9e030fc5a4e8c8e0c0334
BhanuKr <115964594+BhanuKr@users.noreply.github.com>
2023-07-08 09:49:55 +0530

    Update exhibitions.vue
    Three buttons added to the links on top.

--- src/components/scitech/exhibitions.vue
+++ src/components/scitech/exhibitions.vue
@@ -1,214 +1,209 @@
 <script>
-
 </script>
 
 <template>
+    <div class="content-stuff">
 
+        <div class="scitech-navbar">
+            <ul>
+                <li><a href="/pdf/Exhibitions Rules.pdf" class="my_button">Rules</a></li>
+                <li><a href="#coords" class="my_button">Contact</a></li>
+                <li><a href="#hof" class="my_button">Hall of Fame</a></li>
+            </ul>
+        </div>
 
-<div class = "content-stuff">  
+        <div class="intro">
+            <div class="intro-content">
+                <br><br>
+                <h1>Exhibitions</h1><br>
+                <p>From the book to the lab</p><br>
+                <router-link to = "/scitech/exhibitions_register">
+                    <button type="button" class="reg_button">Register</button>
+                </router-link>
+            </div>
+            <div class="intro-image">
+                <img src="/img/scitech/Exhibitions.jpg" alt="scitech">
+            </div>
+        </div>
 
-<div class = "scitech-navbar">
-  <ul>
-    <li><a href="#news">Rules</a></li>
-    <li><a href="#contact">Sample Questions</a></li>
-    <li><a href="#hof">Hall of Fame</a></li>
-  </ul>
-</div> 
+        <div class="declaration">
+            <div class="big-text" style="background-color: #ebf234;">
+                <h1>Don't miss out on <br> IISc's marquee exhibitions event</h1>
+            </div>
+            <div class="small-text" style="background-color: #ebf234;">
+                <p>Here is a chance to challenge your practical skills. Time to have fun playing around and experimenting with ideas, and trying to implement them!
 
-<div class ="intro">
-    <div class = "intro-content">
-        <br><br>
-        <h1>Exhibitions</h1><br>
-        <p>Some stuff to look out for</p>
-    </div>
-    <div class = "intro-image">
-        <img src = "/img/scitech/Exhibitions.jpg" alt = "scitech" width = "100%">
-    </div>
-</div>
-    
-<div class = "declaration">
-    <div class = "big-text" style="background-color: #ebf234;">
-            <h1>Don't miss out on <br> IISc's Premier Chemistry Event</h1>
-    </div>
-    <div class = "small-text" style="background-color: #ebf234;">
-            <p>This is a unique opportunity for any chemistry enthusiast to showcase their problem-solving ability and insight in the subject. The prelims is knocking at the door and this will serve as a gateway to the main Chemenigma event!<br>
+IISc Exhibitions here at Pravega brings to you a one-of-its-kind of contest that would poke your curiosity and make you whack your brains. Identify a real-world problem. , use YOUR brains to think of ideas to solve it and try to realise them!
+</p>
+            </div>
+        </div>
 
-<br>The Final event will truly bring out the Enigma of Chemenigma. The finalists would have to work their way through challenging problems in India's finest research labs with the exciting opportunity to present and defend their work in front of leading Chemists in the panel of judges!
+        <div class="details">
+            <div class="img">
+                <img src="/img/scitech/sample2.png" alt="scitech">
+            </div>
+            <div class="text">
+                <div class="timeline">
+                    <h1>Timeline</h1><br>
+                    <p> Last date of submission of exhibition: 28th July<br>
+                    Final Presentation: Day of Pravega
+                    </p>
+                </div>
+                <div class="eligibility">
+                    <h1>Eligibility</h1><br>
+                    <p> 11th standard to 2nd year undergraduate.
+                    </p>
+                </div>
+                <div class="rules">
+                <h1>Rules</h1>
+                <p>You can find the rules <a href = "/pdf/Exhibitions Rules.pdf">here</a></p>
+                </div>
+                <div class="prizes">
+                    <h1>Prizes</h1><br>
+                    <p> The event will have Prizes of up to 20k.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="coord-intro" id="coords">
+            <h1>Our Coordinators</h1><br>
 
-<br> Do you have what it takes to be in the Finals?</p>
-    </div>    
-</div>
-
-<div class = "details">
-    <div class = "img">
-        <img src = "/img/scitech/sample2.png" alt = "scitech">
-    </div>
-    <div class = "text">
-        <div class = "timeline">
-            <h1>Timeline</h1><br>
-            <p> I'm a paragraph. Click here to add your own text and edit me. It’s easy. 
-                Just click “Edit Text” or double click me to add your own content and make changes to the font. 
-                I’m a great place for you to tell a story and let your users know a little more about you.
+            <p> Meet our amazing coordinators. For any query regarding the event, they are your first point of contact!
+                Don't hesistate to mail them at any time of the day, and they will try to reply as soon as possible. For any general 
+                queries, mail us at scitech[dot]pravega[at]iisc[dot]ac[dot]in.
             </p><br>
         </div>
-        <div class = "eligibility">
-            <h1>Eligibility</h1><br>
-            <p> I'm a paragraph. Click here to add your own text and edit me. It’s easy. 
-                Just click “Edit Text” or double click me to add your own content and make changes to the font. 
-                I’m a great place for you to tell a story and let your users know a little more about you.
-            </p><br>
-        </div>
-        <div class = "prizes">
-            <h1>Prizes</h1><br>
-            <p> I'm a paragraph. Click here to add your own text and edit me. It’s easy. 
-                Just click “Edit Text” or double click me to add your own content and make changes to the font. 
-                I’m a great place for you to tell a story and let your users know a little more about you.
-            </p>
-        </div>
-    </div>    
-</div>
-<div class = "coord-intro">
-    <h1>Our Coordinators</h1><br>
-
-    <p> I'm a paragraph. Click here to add your own text and edit me. It’s easy. 
-        Just click “Edit Text” or double click me to add your own content and make changes to the font. 
-        I’m a great place for you to tell a story and let your users know a little more about you.</p><br>
-</div>
 
 
-<div class = "coords">
-   
-        
-    
-        <div class = "coord">
-            <div class = "coord-img">
-                <img src = "/img/scitech/camelia_photo_chemenigma.jpg" alt = "scitech" height = "120%" width="35%">
+        <div class="coords">
+
+
+
+            <div class="coord">
+                <div class="coord-img">
+                    <img src="\img\scitech\Deep_Exhibitions.jpeg" alt="scitech">
+                </div><br>
+                <div class="coord-text">
+                    <h1>Deep Bhowmik</h1>
+                    <p>Mail: deepbhowmik[at]iisc[dot]ac[dot]in</p>
+                </div>
             </div>
-            <div class = "coord-text">
-                <h1>Some coord</h1>
-                <p>Some stuff</p>
+            <div class="coord">
+                <div class="coord-img">
+                    <img src="\img\scitech\HVD_Exhibitions.jpeg" alt="scitech">
+                </div><br>
+                <div class="coord-text">
+                    <h1>Harsh Vardhan Dewangan</h1>
+                    <p>Mail: vardhand[at]iisc[dot]ac[dot]in</p>
+                </div>
             </div>
         </div>
-        <div class = "coord">
-            <div class = "coord-img">
-                <img src = "/img/scitech/priyanshu_photo_chemenigma.jpeg.jpg" alt = "scitech" height = "100%" width="45%">
-            </div>
-            <div class = "coord-text">
-                <h1>Some coord</h1>
-                <p>Some stuff</p>
+       <!-- <div class="quote">
+            <div class="quote-text" style="font-family: 'Times New Roman', Times, serif;">
+                <p> Understanding reaction intermediates give ideas to new reactions...
+                </p>
             </div>
         </div>
-        <div class = "coord">
-            <div class = "coord-img">
-                <img src = "/img/scitech/camelia_photo_chemenigma.jpg" alt = "scitech" height = "50%" width = "35%">
+
+        <div class="hof" id="hof">
+            <br><br>
+            <div class="big-text-2" style="width:100vw">
+                <h1>Hall of Fame</h1>
             </div>
-            <div class = "coord-text">
-                <h1>Some coord</h1>
-                <p>Some stuff</p>
+            <div class="small-text" style="width:100vw">
+                <table>
+                    <tr>
+                        <th>Position</th>
+                        <th>Names of the winners</th>
+                        <th>College</th>
+                    </tr>
+                    <tr>
+                        <td>1st</td>
+                        <td>Priyanshu Jaseja and Sahil Yadav </td>
+                        <td>MAMC,Delhi and AIIMS,Delhi </td>
+                    </tr>
+                    <tr>
+                        <td>2nd</td>
+                        <td>Supratik Ghosh</td>
+                        <td>RM Vidyamandira Belur</td>
+                    </tr>
+                    <tr>
+                        <td>3rd</td>
+                        <td>Subhajit Chakraborty and Sayantan Khan  </td>
+                        <td>IISER Kolkata</td>
+                    </tr>
+                    <tr>
+                        <td>4th</td>
+                        <td>Snehadeep Gayen and Yeshwanth Narayana </td>
+                        <td>IIT Madras</td>
+                    </tr>
+                </table>
             </div>
+        </div>-->
     </div>
-</div>
-
-<div class = "quote">
-    <div class = "quote-text" style="font-family: 'Times New Roman', Times, serif;">
-        <p> "I'm a paragraph. Click here to add your own text and edit me. It’s easy. 
-            Just click “Edit Text” or double click me to add your own content and make changes to the font. 
-            I’m a great place for you to tell a story and let your users know a little more about you."
-        </p>
-    </div>  
-</div>
-
-<div class ="hof" id = "hof">
-    <br><br>
-    <div class = "big-text-2" style="width:100vw">
-            <h1>Hall of Fame</h1>
-    </div>
-    <div class = "small-text" style = "width:100vw">
-        <table>
-  <tr>
-    <th>Position</th>
-    <th>Names of the winners</th>
-    <th>College</th>
-  </tr>
-  <tr>
-    <td>Alfreds Futterkiste</td>
-    <td>Maria Anders</td>
-    <td>Germany</td>
-  </tr>
-  <tr>
-    <td>Centro comercial Moctezuma</td>
-    <td>Francisco Chang</td>
-    <td>Mexico</td>
-  </tr>
-  <tr>
-    <td>Ernst Handel</td>
-    <td>Roland Mendel</td>
-    <td>Austria</td>
-  </tr>
-  <tr>
-    <td>Island Trading</td>
-    <td>Helen Bennett</td>
-    <td>UK</td>
-  </tr>
-  <tr>
-    <td>Laughing Bacchus Winecellars</td>
-    <td>Yoshi Tannamuri</td>
-    <td>Canada</td>
-  </tr>
-  <tr>
-    <td>Magazzini Alimentari Riuniti</td>
-    <td>Giovanni Rovelli</td>
-    <td>Italy</td>
-  </tr>
-</table>   
-    </div>
-</div>
-</div>  
-
-
-
 </template>
 
 <style scoped>
-
+.reg_button{
+    background-color: #5d83eb; /* Green */
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+  -webkit-transition-duration: 0.4s; /* Safari */
+  transition-duration: 0.4s;
+  border-radius:30px;
+}
+.reg_button:hover{
+    box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
+    background-color: crimson;
+}
 table {
-  font-family: arial, sans-serif;
-  border-collapse: collapse;
-  width: 80%;
-  margin: auto;
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 80%;
+    margin: auto;
 }
 
-td, th {
-  border: 1px solid #dddddd;
-  text-align: left;
-  padding: 8px;
+td,
+th {
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
 }
 
 tr:nth-child(even) {
-  background-color: #dddddd;
+    background-color: #dddddd;
 }
-.scitech-navbar{
-    width:70vw;
+
+.scitech-navbar {
+    width: 70vw;
 }
 
 ul {
-  list-style-type: none;
-  margin-top: 20px;
-  padding: 0;
-  overflow: hidden;
+    list-style-type: none;
+    margin-top: 20px;
+    padding: 0;
+    overflow: hidden;
 }
 
 li {
-  float: right;
+    float: right;
 }
 
 li a {
-  display: block;
-  padding: 20px;
-  background-color: #ffffff;
-  text-decoration: none;
+    display: block;
+    padding: 20px;
+    background-color: #ffffff;
+    text-decoration: none;
 }
-.coord-intro{
+
+.coord-intro {
     position: relative;
     top: 25vh;
     margin: auto;
@@ -217,18 +212,21 @@ li a {
     background-color: #ffffff;
     color: black;
     text-align: center;
+    font-size: larger;
 }
-.content-stuff{
-	position: relative;
-	top: 10vh;
-	height: 90vh;
-	background-color: #ffffff;
-	
-	border-top-left-radius: 25px;
-	border-top-right-radius: 30px;
-	overflow-y: scroll;
+
+.content-stuff {
+    position: relative;
+    top: 10vh;
+    height: 90vh;
+    background-color: #ffffff;
+
+    border-top-left-radius: 25px;
+    border-top-right-radius: 30px;
+    overflow-y: scroll;
 }
-.intro{
+
+.intro {
     display: flex;
     flex-direction: row;
     position: relative;
@@ -240,7 +238,7 @@ li a {
     text-align: center;
 }
 
-.intro-content{
+.intro-content {
     position: relative;
     height: 90vh;
     width: 50vw;
@@ -248,7 +246,14 @@ li a {
     align-items: center;
     text-align: center;
 }
-.intro-image{
+.intro-content h1{
+    font-size: 5rem;
+}
+.intro-content p{
+    font-size: 3rem;
+}
+
+.intro-image {
     position: relative;
     height: 90vh;
     width: 50vw;
@@ -257,7 +262,14 @@ li a {
     text-align: center;
 }
 
-.declaration{
+.intro-image img {
+    width: 80%;
+    background-color: #ffffff;
+    align-items: center;
+    text-align: center;
+}
+
+.declaration {
     display: flex;
     flex-direction: row;
     position: relative;
@@ -269,7 +281,7 @@ li a {
     text-align: center;
 }
 
-.quote{
+.quote {
     height: auto;
     margin: auto;
     width: 80vw;
@@ -278,7 +290,7 @@ li a {
     text-align: center;
 }
 
-.quote-text{
+.quote-text {
     position: relative;
     height: auto;
     width: 80vw;
@@ -289,7 +301,7 @@ li a {
     text-align: center;
 }
 
-.big-text{
+.big-text {
     position: relative;
     height: 45vh;
     width: 50vw;
@@ -299,7 +311,8 @@ li a {
     align-items: center;
     text-align: center;
 }
-.big-text-2{
+
+.big-text-2 {
     position: relative;
     height: 15vh;
     width: 50vw;
@@ -307,9 +320,10 @@ li a {
     margin-right: 8vw;
     background-color: #ffffff;
     align-items: center;
-    text-align: center; 
+    text-align: center;
 }
-.small-text{
+
+.small-text {
     font-size: 1.2vw;
     position: relative;
     height: 45vh;
@@ -320,7 +334,7 @@ li a {
     text-align: center;
 }
 
-.details{
+.details {
     display: flex;
     flex-direction: row;
     position: relative;
@@ -332,7 +346,7 @@ li a {
     text-align: center;
 }
 
-.text{
+.text {
     display: flex;
     flex-direction: column;
     height: 90vh;
@@ -342,19 +356,22 @@ li a {
     align-items: left;
     text-align: left;
 }
-.timeline,.eligibility,.prizes{
+
+.timeline, .rules,
+.eligibility,
+.prizes {
     margin-bottom: 7vh;
     padding-right: 2vw;
 }
 
-.details img{
-    height:90vh;
-    width:50vw;
+.details img {
+    height: 90vh;
+    width: 50vw;
 }
 
-.coords{
+.coords {
     position: relative;
-    top: 10vh;
+    top: 16vh;
     height: 90vh;
     width: 100vw;
     display: flex;
@@ -366,14 +383,22 @@ li a {
     text-align: center;
 }
 
-.coord{
+.coord {
     width: 30vw;
     background-color: #ffffff;
     align-items: right;
     text-align: center;
 }
 
-.content{
+.coord-img img{
+    width:250px;
+    height:300px;
+    border-radius: 10px;
+    box-shadow: 5px 5px 5px grey;
+    object-fit: cover;
+}
+
+.content {
     display: flex;
     flex-direction: row;
     align-items: center;
@@ -384,7 +409,7 @@ li a {
     text-align: center;
 }
 
-.hof{
+.hof {
     position: relative;
     top: 10vh;
     height: 40vh;
@@ -393,31 +418,77 @@ li a {
     align-items: center;
     text-align: center;
 }
+    
+.my_button {
+  display: inline-block;
+  padding: 10px 20px;
+  background-color: #4CAF50;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
 
+.my_button:hover {
+  background-color: #45a049;
+}
+
+.my_button:active {
+  background-color: #3e8e41;
+
+}  
+    
 @media screen and (max-width:1024px) {
-    .intro,.details,.declaration,.content,.hof{
+
+    .intro,
+    .details,
+    .declaration,
+    .content,
+    .hof {
         flex-direction: column;
     }
 
-    .scitech-navbar{
+    .scitech-navbar {
         width: 87vw;
-       justify-content: center;
+        justify-content: center;
     }
 
-    .scitech-navbar li{
-        float:right;
+    .scitech-navbar li {
+        float: right;
     }
-    .declaration,.content,.hof{
+
+    .declaration,
+    .content,
+    .hof {
         height: auto;
         border-radius: 10px;
     }
-    .intro-image,.intro-content,.text{
+
+    .intro-image,
+    .intro-content,
+    .text {
         width: 100vw;
     }
-    .intro-content{
+
+    .intro-image img {
+        width: 90%;
+    }
+
+    .intro{
+        height: 100vh;
+    }
+
+    .intro-content {
         width: 100vw;
     }
-    .big-text,.big-text-2{
+    .intro-content h1{
+        font-size: 3rem;
+    }
+    .intro-content p{
+        font-size: 2rem;
+    }
+
+    .big-text{
         margin-top: 2vh;
         margin-bottom: 5vh;
         margin-left: 8vw;
@@ -425,64 +496,90 @@ li a {
         width: 90vw;
         height: auto;
     }
-    .small-text{
+
+    .big-text-2 {
+        margin-top: 2vh;
+        margin-bottom: 5vh;
+        padding-top: 75px;
+        width: 90vw;
+        height: auto;
+    }
+
+    .small-text {
         margin-top: 2vh;
         margin-bottom: 9vh;
         width: 90vw;
-        height:auto;
+        height: auto;
         font-size: 5vw;
     }
 
-    .details img{
+    .details img {
         width: 100vw;
         height: auto;
-        
+
     }
-    .text,.timeline,.eligibility,.prizes{
+
+    .text,
+    .timeline, .rules,
+    .eligibility,
+    .prizes {
         padding: 30px;
         text-align: center;
-        margin: 0%;
+        margin: -1%;
     }
-    .text{
+
+    .text {
         background-color: beige;
         margin-top: 3vh;
         border-radius: 10px;
+        height:auto;
     }
-    .coord-intro{
+
+    .coord-intro {
         width: 90vw;
         height: auto;
         position: relative;
-        top:15%;
+        top: 15%;
         margin-top: 3vh;
         margin-bottom: 5vh;
     }
-    .coords{
+
+    .coords {
         position: relative;
-        top: 32%;
+        top: 20%;
         flex-direction: column;
         justify-content: center;
     }
-    .coord{
+
+    .coord {
         width: 90vw;
         height: auto;
         margin-top: 3vh;
     }
-    .coord-img img{
+
+    .coord-img img {
         width: 45vw;
-        height: auto;
+        height: 60vw;
     }
-    .hof{
+
+    .hof {
         position: relative;
-        top: 53%;
+        top: 40%;
         height: auto;
         text-align: center;
         width: 100vw;
         margin: auto;
     }
 
-    .hof .big-text{
+    .hof .big-text {
         padding-top: 30px;
-        margin:auto;
+        margin: auto;
     }
-}
-</style>
+
+    .quote{
+        position: relative;
+        top: 40%;
+        height: auto;
+        margin: auto;
+    }
+}</style>
